adda8aeec6f5a6b105a9a0c28751b1992df87390
GennadiyPob <gennadiy.pob@gmail.com>
2023-11-03 22:12:07 +0200

    Update

---
+++ storage.py
@@ -0,0 +1,75 @@
+import json
+import yaml
+import pathlib
+
+
+class Storage:
+    def get_value(self, key):
+        raise NotImplementedError
+
+
+class JSONStorage(Storage):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def get_value(self, key):
+        with open(self.filename, "r", encoding='utf-8') as fd:
+            data = json.load(fd)
+            return data.get(key, None)
+
+
+class YAMLStorage(Storage):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def get_value(self, key):
+        with open(self.filename, "r", encoding='utf-8') as fd:
+            data = yaml.load(fd, Loader=yaml.FullLoader)
+            return data.get(key, None)
+
+# import json
+# import yaml
+# import pathlib
+
+
+# class Storage:
+#     def get_value(self, key):
+#         raise NotImplementedError
+
+
+# class JSONStorage(Storage):
+#     def __init__(self, filename):
+#         self.filename = filename
+
+#     def get_value(self, key):
+#         with open(self.filename, 'r', encoding='utf-8') as fd:
+#             data = json.load(fd)
+#             return data.get(key)
+
+
+# class YAMLStorage(Storage):
+#     def __init__(self, filename):
+#         self.filename = filename
+
+#     def get_value(self, key):
+#         with open(self.filename, 'r', encoding='utf-8') as fd:
+#             data = yaml.load(fd, Loader=yaml.FullLoader)
+#             return data.get(key)
+
+
+class Service:
+    def __init__(self, storage: Storage):
+        self.storage = storage
+
+    def get(self, key):
+        return self.storage.get_value(key)
+
+
+if __name__ == '__main__':
+    storage_json = JSONStorage(pathlib.Path() / 'data.json')
+    service_one = Service(storage_json)
+    print(service_one.get('name'))
+
+    storage_yaml = YAMLStorage(pathlib.Path() / 'data.yaml')
+    service_two = Service(storage_yaml)
+    print(service_two.get('name'))
